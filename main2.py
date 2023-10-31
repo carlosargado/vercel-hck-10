5af264d2f000fdbc0435ae52048467b802705547
carlosargado <147989300+carlosargado@users.noreply.github.com>
2023-10-31 14:37:40 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,32 @@
+# import FastApi
+from fastapi import FastAPI, Header, HTTPException
+
+app = FastAPI()
+
+# request
+# - url
+# - header
+# - key
+# - body
+# - json
+
+# key yang perlu dimasukkan ke dalam header
+key = "hacktiv8mania2023"
+
+#public
+@app.get("/")
+def helloFunction():
+    return {
+        "message" : "hello world"
+    }
+
+#secret -> harus memasukan authentication
+@app.get("/secret")
+def helloFunction(api_key: str = Header(None)):
+    # check api_key dari header
+    if api_key is None or api_key != key:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+    
+    return {
+        "message" : "secret message"
+    }
